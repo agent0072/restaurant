cba1a94690a53c566798c758b527d786acbbadea
iga krawczyk <igulakrawczyk@gmail.com>
2022-09-30 18:13:16 +0200

    zmienne i metody

---
+++ src/main/java/iga/krawczyk/App.java
@@ -0,0 +1,22 @@
+package iga.krawczyk;
+
+public class App {
+
+    public static void main(String[] args) {
+        printlnmassage( "2022/2023");
+    }
+
+
+    static void printlnmassage(String year) {
+
+        System.out.println("Witajcie drodzy klienci");
+        System.out.println("w roku " + year);
+        System.out.println("zapoznajcie się z naszym menu");
+        System.out.println("zapraszamy w dzień i w nocy");
+        System.out.println("dania na cipło i na zimno");
+        System.out.println("przygotowujemy dania włoskie i hiszpańskie ");
+        System.out.println("w roku " + year + " dania będą unikatowe");
+
+
+    }
+}
